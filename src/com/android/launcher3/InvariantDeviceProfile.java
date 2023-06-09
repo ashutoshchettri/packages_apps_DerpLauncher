b5dff8e4bb10134d5ad31d7402f5044579a85a62
Pranav Vashi <neobuddy89@gmail.com>
2023-06-09 15:30:45 +0545

    Launcher3: Add pref to set app drawer row height

--- src/com/android/launcher3/InvariantDeviceProfile.java
+++ src/com/android/launcher3/InvariantDeviceProfile.java
@@ -378,6 +378,7 @@ public class InvariantDeviceProfile implements OnSharedPreferenceChangeListener 
             case KEY_FONT_SIZE:
             case IconDatabase.KEY_ICON_PACK:
             case KEY_MAX_LINES:
+	    case DeviceProfile.KEY_ROW_HEIGHT:
                 onConfigChanged(mContext);
                 break;
         }
